b43f445111da2badc6f2870c4edd7be4c9371100
isaac1335 <isaacfernandez1335@gmail.com>
2023-12-01 10:37:35 +0100

    hoteles actualizado

--- app/src/main/java/com/example/intents/ClHotel.java
+++ app/src/main/java/com/example/intents/ClHotel.java
@@ -1,4 +1,45 @@
 package com.example.intents;
 
-public class ClHotel {
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class HotelsActivity extends AppCompatActivity {
+
+    Button btEmail1, btWeb1, btTelefon1;
+    Button btEmail2, btWeb2, btTelefon2;
+    Button btEmail3, btWeb3, btTelefon3;
+    ImageView imgHotel1, imgHotel2, imgHotel3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotels);
+
+
+        imgHotel1 = findViewById(R.id.imgHotel1);
+        imgHotel2 = findViewById(R.id.imgHotel2);
+        imgHotel3 = findViewById(R.id.imgHotel3);
+
+        Glide.with(this)
+                .load("")
+                .error(R.drawable.ic_error_foreground)
+                .into(imgHotel1);
+
+        Glide.with(this)
+                .load("")
+                .error(R.drawable.ic_error_foreground)
+                .into(imgHotel2);
+
+        Glide.with(this)
+                .load("")
+                .error(R.drawable.ic_error_foreground)
+                .into(imgHotel3);
+    }
+
 }
