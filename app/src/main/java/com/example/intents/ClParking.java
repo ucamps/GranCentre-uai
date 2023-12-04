08b54f2e79e8207e6d69df010e90f997b8efca93
isaac1335 <isaacfernandez1335@gmail.com>
2023-12-04 12:24:18 +0100

    fotos tamaño adecuado

--- app/src/main/java/com/example/intents/ClParking.java
+++ app/src/main/java/com/example/intents/ClParking.java
@@ -1,4 +1,32 @@
 package com.example.intents;
 
-public class ClParking {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClParking extends AppCompatActivity implements View.OnClickListener{
+
+    ImageView imgmatparc;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.parking);
+
+        imgmatparc = findViewById(R.id.matparc);
+        imgmatparc.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.matparc) {
+            Intent pag = new Intent(this, MainActivity.class);
+            startActivity(pag);
+        }
+    }
 }
