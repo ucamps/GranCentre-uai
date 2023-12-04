b2de86fb01e41df375f83214ed30e5e91787e15f
isaac1335 <isaacfernandez1335@gmail.com>
2023-12-04 08:07:50 +0100

    hoteles actualizado 12:28

--- app/src/main/java/com/example/intents/ClHotel.java
+++ app/src/main/java/com/example/intents/ClHotel.java
@@ -1,20 +1,34 @@
 package com.example.intents;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
-public class HotelsActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    Button btEmail1, btWeb1, btTelefon1;
-    Button btEmail2, btWeb2, btTelefon2;
-    Button btEmail3, btWeb3, btTelefon3;
+public class ClHotel extends AppCompatActivity implements View.OnClickListener{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotels);
+        setupSpinnerBasic();
+    }
+
+    private void setupSpinnerBasic() {
+        Spinner spinner = findViewById(R.id.spinner_hotels);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.valoracio_hotels, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
     ImageView imgHotel1, imgHotel2, imgHotel3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +54,28 @@ public class HotelsActivity extends AppCompatActivity {
                 .load("")
                 .error(R.drawable.ic_error_foreground)
                 .into(imgHotel3);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.email1) {
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("mailto:exemple@gmail.com"));
+            startActivity(intent);
+        }
+
+        if (v.getId() == R.id.web1) {
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse(""));
+            startActivity(intent);
+        }
+
+        if (v.getId() == R.id.telefon1) {
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("tel:647328384"));
+            startActivity(intent);
+        }
+
+        if (v.getId() == R.id.ubi1) {
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("geo:41.3, 20.3"));
+            startActivity(intent);
+        }
     }
 
 }
