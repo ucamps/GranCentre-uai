7a3fe6f7d3d9091c7c1b1f8e7a7480b17a5b7a6a
isaac1335 <isaacfernandez1335@gmail.com>
2023-12-04 09:07:23 +0100

    holaa

--- app/src/main/java/com/example/intents/ClHotel.java
+++ app/src/main/java/com/example/intents/ClHotel.java
@@ -13,22 +13,9 @@ import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+
 public class ClHotel extends AppCompatActivity implements View.OnClickListener{
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hotels);
-        setupSpinnerBasic();
-    }
-
-    private void setupSpinnerBasic() {
-        Spinner spinner = findViewById(R.id.spinner_hotels);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.valoracio_hotels, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-    }
-
     ImageView imgHotel1, imgHotel2, imgHotel3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,19 +50,66 @@ public class ClHotel extends AppCompatActivity implements View.OnClickListener{
         }
 
         if (v.getId() == R.id.web1) {
-            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse(""));
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://www.guestreservations.com/urh-ciutat-de-mataro/booking?gad_source=1&gclid=Cj0KCQiA67CrBhC1ARIsACKAa8QPE9xacCPQS9WMg0U-1ln3zcpxzodNtoKDf4RSwH2emV-BYiXk8EAaAi27EALw_wcB"));
             startActivity(intent);
         }
 
         if (v.getId() == R.id.telefon1) {
-            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("tel:647328384"));
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("tel:911238107"));
             startActivity(intent);
         }
 
         if (v.getId() == R.id.ubi1) {
-            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("geo:41.3, 20.3"));
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("geo:41.53153167128203, 2.436998096397077"));
             startActivity(intent);
         }
+        if (v.getId() == R.id.email2) {
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("mailto:exemple@gmail.com"));
+            startActivity(intent);
+        }
+
+        if (v.getId() == R.id.web2) {
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://www.guestreservations.com/new-hotel-colon/booking?gad_source=1&gclid=Cj0KCQiA67CrBhC1ARIsACKAa8QU0fFsvgFnSXt6H2OzcmmyO8WxPkr0otyvh44MPUwHEGpF-Tqov4EaAjPBEALw_wcB"));
+            startActivity(intent);
+        }
+
+        if (v.getId() == R.id.telefon2) {
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("tel:911238107"));
+            startActivity(intent);
+        }
+
+        if (v.getId() == R.id.ubi2) {
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("geo:41.53795268216341, 2.4488264591326594"));
+            startActivity(intent);
+        }
+
+        if (v.getId() == R.id.email3) {
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("mailto:esp-rrhh@hotelbb.com"));
+            startActivity(intent);
+        }
+
+        if (v.getId() == R.id.web3) {
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://www.hotel-bb.com/es/hotel/barcelona-mataro?utm_source=google&utm_medium=cpc&utm_campaign=es_goo_brand_text_MaxConv_mix_Barcelona&utm_source=google&utm_medium=voila&utm_campaign=ES_Search_Generico_Barcelona&gad_source=1&gclid=Cj0KCQiA67CrBhC1ARIsACKAa8Q3B7ol6YT0FsnMPQKq3U2rd549N2fvDGi0fKmd0WyjXlNCJLsS7UoaAkYREALw_wcB"));
+            startActivity(intent);
+        }
+
+        if (v.getId() == R.id.telefon3) {
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("tel:900 533 147"));
+            startActivity(intent);
+        }
+
+        if (v.getId() == R.id.ubi3) {
+            Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("geo:41.527517971282464, 2.433522767561614"));
+            startActivity(intent);
+        }
+    }
+
+    private void setupSpinnerBasic() {
+        Spinner spinner = findViewById(R.id.spinner_hotels);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.valoracio_hotels, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
 }
